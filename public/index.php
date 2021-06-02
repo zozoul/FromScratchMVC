d99086a0f22d998e0ec939d846dd4dbc0ba74a0a
Herizo Rasolomampionona <hrz.rasolo@gmail.com>
2021-06-02 21:46:25 +0300

    2021-06-02 [FIX] (Zo) Gestion route

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<?php
+/*error_reporting(E_ALL);
+ini_set('display_errors',1);*/
+require '../vendor/autoload.php';
+$router = new AltoRouter();
+$router->setBasePath('/FromScratchMVC/public');
+$router->map('GET', '/', 'home');
+$router->map('GET', '/contact', 'contact', 'contact');
+$router->map('GET', '/blog/[*:slug]-[i:id]', 'blog/article', 'article');
+
+$match = $router->match();
+
+require '../templates/header.php';
+
+if (is_array($match)) {
+    $params = $match['params'];
+    var_dump($match['target']);
+    //require "../templates/{$match['target']}.php";
+    require "../templates/ok.php";
+} else {
+    require "../templates/404.php";
+}
+
+require '../templates/footer.php';
